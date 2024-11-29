5669da2986f076e7ebd156edeaa614cd9239622f
Ayrik Nabirahni <ayrytube@gmail.com>
2024-11-29 15:03:45 -0800

    version bumped to 2024.11.29.1500.alpha

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ import time
 from KermLib import *
 from ascii import *
 
-version = '2024.11.29.0130.alpha'
+version = '2024.11.29.1500.alpha'
 
 class BasketballPlayer():
     def __init__(self, name, position, positionnumber, team, threept, passing, drivinglay, tov, perd, intd, interception, passpref, possession, defender, player, points_made, passes_made, interceptions_made):
