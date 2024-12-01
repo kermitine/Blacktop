526bd200c57ec49026db9ae3ef055b20029bea16
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2024-11-30 22:19:44 -0800

    version bumped to 2024.11.30.2220.unstable

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ import time
 from KermLib.KermLib import *
 from basketball_ascii import *
 
-version = '2024.11.30.2030.unstable'
+version = '2024.11.30.2220.unstable'
 
 class BasketballPlayer():
     def __init__(self, name, position, positionnumber, team, threept, passing, drivinglay, tov, perd, intd, interception, passpref, possession, defender, player, points_made, passes_made, interceptions_made):
