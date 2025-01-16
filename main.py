6e70488f6e254f5d9cf87e7f1925afc69f9bd01f
Ayrik Nabirahni <ayrytube@gmail.com>
2025-01-16 10:40:36 -0800

    version bumped to 2025.1.16.1040.stable

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from KermLib.KermLib import *
 from vars.basketball_ascii import *
 
-version = '2025.1.5.1445.stable'
+version = '2025.1.16.1040.stable'
 
 
 # TEAM AND PLAYER DATA ARE LOADED FROM PLAYERS_AND_TEAMS, DONT WORRY IF EDITOR SAYS THAT VARIABLES ARE UNRECOGNIZED
